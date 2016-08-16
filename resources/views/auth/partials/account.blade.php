14baf1574fdd5c0f360134471ecd267e636d353e
Tjoosten <Tjoosten3@gmail.com>
2016-08-16 02:28:28 +0200

    Add account information settings partial

---
+++ resources/views/auth/partials/account.blade.php
@@ -0,0 +1,39 @@
+<div class="panel panel-default">
+    <div class="panel-heading">Account Information</div>
+
+    <div class="panel-body">
+        <form class="form-horizontal" method="POST" action="">
+
+            {{-- Name form-group --}}
+            <div class="form-group">
+                <label for="username" class="col-sm-2 control-label">
+                    Username: <span class="text-danger">*</span>
+                </label>
+
+                <div class="col-md-8">
+                    <input type="text" class="form-control" name="name" value="{{ auth()->user()->name }}" placeholder="Your Account name" />
+                </div>
+            </div>
+
+            {{-- Email form-group --}}
+            <div class="form-group">
+                <label for="" class="col-sm-2 control-label">
+                    Email Address: <span class="text-danger">*</span>
+                </label>
+
+                <div class="col-sm-8">
+                    <input type="email" class="form-control" name="email" value="{{ auth()->user()->email }}" placeholder="Your email address">
+                </div>
+            </div>
+
+            {{-- SUBMIT & REST form-group --}}
+            <div class="form-group">
+                <div class="col-sm-10 col-sm-offset-2">
+                    <button type="submit" class="btn btn-primary">Save</button>
+                    <button type="reset" class="btn btn-danger">Cancel</button>
+                </div>
+            </div>
+
+        </form>
+    </div>
+</div>
---
+++ resources/views/auth/profile.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            {{-- Menu --}}
+            <div class="col-md-3">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Menu</div>
+
+                    <div class="list-group">
+                        <a href="#account" aria-controls="profile" role="tab" data-toggle="tab" class="list-group-item">Account information</a>
+                        <a href="#sec" aria-controls="profile" role="tab" data-toggle="tab" class="list-group-item">Security information</a>
+                        <a href="#api" aria-controls="profile" role="tab" data-toggle="tab" class="list-group-item">API information</a>
+                    </div>
+                </div>
+            </div>
+            {{-- End menu --}}
+
+            {{-- Content --}}
+            <div class="col-md-9">
+                <div class="tab-content">
+                    <div role="tabpanel" class="tab-pane fade in active" id="account">
+                        @include('auth.partials.account')
+                    </div>
+
+                    <div role="tabpanel" class="tab-pane fade in" id="sec">
+                        @include('auth.partials.security')
+                    </div>
+
+                    <div role="tabpanel" class="tab-pane fade in" id="api">
+                        @include('auth.partials.api')
+                    </div>
+                </div>
+            </div>
+            {{-- End content --}}
+        </div>
+    </div>
+@endsection
