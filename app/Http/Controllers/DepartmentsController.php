8bacc77d87d85d5e6745d3477350196b25f6f7af
Tjoosten <Tjoosten3@gmail.com>
2016-08-22 04:58:12 +0200

    Add departments controller

---
+++ app/Http/Controllers/DepartmentsController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+/**
+ * Class DepartmentsController
+ * @package App\Http\Controllers
+ */
+class DepartmentsController extends Controller
+{
+    /**
+     * DepartmentsController constructor.
+     */
+    public function __construct()
+    {
+    }
+
+    public function index()
+    {
+
+    }
+
+    public function show($did)
+    {
+
+    }
+
+
+}
